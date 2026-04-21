0582e1772787a934a2ddf68a1746c121d46887ec
omegayy <150604609+omegayy@users.noreply.github.com>
2026-04-21 03:15:37 +0200

    Update songs.lua

--- songs.lua
+++ songs.lua
@@ -1,4 +1,5 @@
 return {
     {Name = "Shake That - Eminem", Artist = "Eminem", URL = "https://github.com/omegayy/Internals/raw/refs/heads/main/Shake%20That%20-%20Eminem.mp3"},
-    {Name = "Gummo - 6ix9ine", Artist = "Eminem", URL = "https://github.com/omegayy/Internals/raw/refs/heads/main/GUMMO%20-%206ix9ine.mp3"},
+    {Name = "Gummo - 6ix9ine", Artist = "6ix9ine", URL = "https://github.com/omegayy/Internals/raw/refs/heads/main/GUMMO%20-%206ix9ine.mp3"},
+    {Name = "KINDA HOMELESS FUNK", Artist = "IDFK", URL = "https://github.com/omegayy/Internals/raw/refs/heads/main/KINDA%20HOMELESS%20FUNK.mp3"},
 }
