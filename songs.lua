88474200bf1bdbba3abf37a2a17b29f90b94e8b7
omegayy <150604609+omegayy@users.noreply.github.com>
2026-04-21 15:03:56 +0200

    Update songs.lua

--- songs.lua
+++ songs.lua
@@ -2,4 +2,6 @@ return {
     {Name = "Shake That - Eminem", Artist = "Eminem", URL = "https://github.com/omegayy/Internals/raw/refs/heads/main/Shake%20That%20-%20Eminem.mp3"},
     {Name = "Gummo - 6ix9ine", Artist = "6ix9ine", URL = "https://github.com/omegayy/Internals/raw/refs/heads/main/GUMMO%20-%206ix9ine.mp3"},
     {Name = "KINDA HOMELESS FUNK", Artist = "IDFK", URL = "https://github.com/omegayy/Internals/raw/refs/heads/main/KINDA%20HOMELESS%20FUNK.mp3"},
+    {Name = "Häschenparty - Schnuffel", Artist = "Schnuffel", URL = "https://github.com/omegayy/Internals/raw/refs/heads/main/H%C3%A4schenparty%20-%20Schnuffel.mp3"},
+
 }
